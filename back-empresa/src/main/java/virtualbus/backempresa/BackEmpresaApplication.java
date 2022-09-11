3deb97b8b71101befbedb967b15c57fb82964dca
BOSONIT\robin.bucad <robin.bucad6@gmail.com>
2022-09-11 18:04:23 +0200

    eureka client

--- back-empresa/src/main/java/virtualbus/backempresa/BackEmpresaApplication.java
+++ back-empresa/src/main/java/virtualbus/backempresa/BackEmpresaApplication.java
@@ -2,6 +2,7 @@ package virtualbus.backempresa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class BackEmpresaApplication {
 	@Bean
 	PasswordEncoder passwordEncoder(){
--- back-web/src/main/java/virtualbus/backweb/BackWebApplication.java
+++ back-web/src/main/java/virtualbus/backweb/BackWebApplication.java
@@ -2,10 +2,12 @@ package virtualbus.backweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class BackWebApplication {
 
 	public static void main(String[] args) {
