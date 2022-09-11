00cc7cabe5444368e3f4d60a70fa6eb34659604b
BOSONIT\robin.bucad <robin.bucad6@gmail.com>
2022-09-11 18:40:54 +0200

    hystrix

--- back-web/src/main/java/virtualbus/backweb/client/ReservasRealizadasHystrixFB.java
+++ back-web/src/main/java/virtualbus/backweb/client/ReservasRealizadasHystrixFB.java
@@ -2,15 +2,19 @@ package virtualbus.backweb.client;
 
 
 import antlr.Token;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class ReservasRealizadasHystrixFB implements ReservasRealizadasClient{
+
+    Logger LOGGER = LoggerFactory.getLogger(ReservasRealizadasHystrixFB.class);
     @Override
     public ResponseEntity<String> checkToken(String token) {
-        String tokenHystrix = " Otro token ";
-        return ResponseEntity.ok(tokenHystrix);
+        LOGGER.info(String.format("Error check token server empresa not up "));
+        return ResponseEntity.ok("Error check token server empresa not up ");
     }
 }
