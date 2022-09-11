5072075cddcc5481d0d895eb815fd18fb9a5b11f
BOSONIT\robin.bucad <robin.bucad6@gmail.com>
2022-09-11 18:45:48 +0200

    hystrix

--- back-empresa/src/main/java/virtualbus/backempresa/BackEmpresaApplication.java
+++ back-empresa/src/main/java/virtualbus/backempresa/BackEmpresaApplication.java
@@ -3,6 +3,7 @@ package virtualbus.backempresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableHystrix
 public class BackEmpresaApplication {
 	@Bean
 	PasswordEncoder passwordEncoder(){
